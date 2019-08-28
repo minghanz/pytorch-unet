15a8c572dca00780e5f6844b9c4c2ce33bd364e8
Minghan Zhu <minghanz@umich.edu>
2019-08-28 15:38:37 -0400

    Change unet for high dimension feature output

--- unet.py
+++ unet.py
@@ -55,14 +55,12 @@ class UNet(nn.Module):
         self.up_path = nn.ModuleList()
         for i in reversed(range(depth - 1)):
             self.up_path.append(
-                UNetUpBlock(prev_channels, 2 ** (wf + i), up_mode, padding, batch_norm)
+                # UNetUpBlock(prev_channels, 2 ** (wf + i), up_mode, padding, batch_norm)
+                UNetUpBlockDeep(prev_channels, 2 ** (wf + i), prev_channels, up_mode, padding, batch_norm)
+                
             )
-            prev_channels = 2 ** (wf + i)
+            # prev_channels = 2 ** (wf + i)
 
-        # block = []
-        # block.append(nn.Conv2d(prev_channels, n_classes, kernel_size=1))
-        # block.append(nn.ReLU())
-        # self.last = nn.Sequential(*block)
         self.last = nn.Conv2d(prev_channels, n_classes, kernel_size=1)
 
     def forward(self, x):
@@ -113,6 +111,36 @@ class UNetUpBlock(nn.Module):
             )
 
         self.conv_block = UNetConvBlock(in_size, out_size, padding, batch_norm)
+
+    def center_crop(self, layer, target_size):
+        _, _, layer_height, layer_width = layer.size()
+        diff_y = (layer_height - target_size[0]) // 2
+        diff_x = (layer_width - target_size[1]) // 2
+        return layer[
+            :, :, diff_y : (diff_y + target_size[0]), diff_x : (diff_x + target_size[1])
+        ]
+
+    def forward(self, x, bridge):
+        up = self.up(x)
+        crop1 = self.center_crop(bridge, up.shape[2:])
+        out = torch.cat([up, crop1], 1)
+        out = self.conv_block(out)
+
+        return out
+
+# create this class because the original unet will have fewer dimensions as it goes deeper, but we want a feature output of high dimensions
+class UNetUpBlockDeep(nn.Module):
+    def __init__(self, in_size, skip_in_size, out_size, up_mode, padding, batch_norm):
+        super(UNetUpBlockDeep, self).__init__()
+        if up_mode == 'upconv':
+            self.up = nn.ConvTranspose2d(in_size, in_size, kernel_size=2, stride=2)
+        elif up_mode == 'upsample':
+            self.up = nn.Sequential(
+                nn.Upsample(mode='bilinear', scale_factor=2),
+                nn.Conv2d(in_size, in_size, kernel_size=1),
+            )
+
+        self.conv_block = UNetConvBlock(in_size + skip_in_size, out_size, padding, batch_norm)
 
     def center_crop(self, layer, target_size):
         _, _, layer_height, layer_width = layer.size()
